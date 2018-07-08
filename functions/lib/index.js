e508dce1c3daa1d4f706d3bff947ab23fc18ea8e
K-Coffee <hekouki111@gmail.com>
2018-07-08 12:04:27 +0900

    add filter

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,24 +3,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const comment_1 = require("./models/comment");
+const moment = require("moment");
 // init data
 admin.initializeApp();
 const firebase = admin.database();
 const commentReference = firebase.ref('/comments');
+const dayOfMillSeconds = 1000 * 60 * 60 * 24;
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 exports.helloWorld = functions.https.onRequest((request, response) => {
     // get comments.
     commentReference.once('value')
-        .then(snapshots => {
-        const comments = [];
-        Object.keys(snapshots.val()).forEach(key => {
-            comments.push(new comment_1.Comment(snapshots.val()[key]).setData(key));
-        });
-        return comments;
-    })
+        .then(snapshots => toComments(snapshots))
+        .then(comments => comments.filter(comment => {
+        return moment().diff(moment(comment.date)) > dayOfMillSeconds;
+    }))
         .then(comments => {
-        response.send(comments[0].content + ' is message');
+        response.send(comments[0].user.name + ' is Over Day ');
     });
 });
+function toComments(snapshots) {
+    const comments = [];
+    Object.keys(snapshots.val()).forEach(key => {
+        comments.push(new comment_1.Comment(snapshots.val()[key]).setData(key));
+    });
+    return comments;
+}
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { Comment } from './models/comment';
+import * as moment from 'moment';
 
 // init data
 admin.initializeApp();
 const firebase = admin.database();
 const commentReference = firebase.ref('/comments');
+const dayOfMillSeconds = 1000 * 60 * 60 * 24;
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -13,14 +15,19 @@ const commentReference = firebase.ref('/comments');
 export const helloWorld = functions.https.onRequest((request, response) => {
     // get comments.
     commentReference.once('value')
-        .then(snapshots => {
-            const comments: Comment[] = [];
+        .then(snapshots => toComments(snapshots))
+        .then(comments => 
+            comments.filter(comment => {
+                return moment().diff(moment(comment.date)) > dayOfMillSeconds;}))
+        .then(comments => {
+            response.send(comments[0].user.name + ' is Over Day ');
+        });
+    });
+
+function toComments(snapshots: any): Comment[] {
+    const comments: Comment[] = [];
             Object.keys(snapshots.val()).forEach(key => {
                 comments.push(new Comment(snapshots.val()[key]).setData(key));
             });
             return comments;
-        })
-        .then(comments => {
-            response.send(comments[0].content + ' is message');
-        });
-    });
+}
